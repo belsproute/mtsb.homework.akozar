208795b5295cc61e6858c7ba46b851271abf6008
belsprout <kozar.a.a.rk6@yandex.ru>
2024-04-06 20:59:06 +0300

    test diff

--- src/main/java/NewBranchTest.java
+++ src/main/java/NewBranchTest.java
@@ -1,6 +1,7 @@
 public class NewBranchTest {
     public static void OneMoreHello(){
         System.out.println("New class for new branch-New class for new branch");
+        System.out.println("New example sout");
     }
 
     public static void main(String[] args) {
