dc9c7fc30d0b362883f443b261dacda586c40a3c
belsprout <kozar.a.a.rk6@yandex.ru>
2024-04-06 21:08:46 +0300

    test diff 2

--- src/main/java/NewBranchTest.java
+++ src/main/java/NewBranchTest.java
@@ -2,7 +2,7 @@ public class NewBranchTest {
     public static void OneMoreHello(){
         System.out.println("New class for new branch-New class for new branch");
         System.out.println("New example sout");
-        System.out.println("New example sout for merge types example");
+        System.out.println("New example sout for merge types example 1");
     }
 
     public static void main(String[] args) {
