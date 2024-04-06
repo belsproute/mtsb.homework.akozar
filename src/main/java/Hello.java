116bb4b3b0b4d494f4b6c6b874c6513a67b459c3
belsprout <kozar.a.a.rk6@yandex.ru>
2024-04-06 17:00:13 +0300

    init commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+
+    public static void sayHello(){
+        System.out.println("Привет, мир!");
+    }
+
+    public static void main(String[] args) {
+        sayHello();
+    }
+}
